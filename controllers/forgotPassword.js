03d81fc9be6e3bd125974dacd8dbed40791be812
coder1110111 <ayush.yadav.dev610@gmail.com>
2025-02-05 19:50:31 +0530

    Used axios to manage to finally send a mail to a user

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -1,32 +1,43 @@
-const sib = require('sib-api-v3-sdk');
 require('dotenv').config();
+const axios = require('axios');
+
+
+
 const User = require('../models/user');
 
 exports.SendLink = async (req,res) => {
     const { email } = req.body;
+    //console.log(email);
     try {
-        console.log('Terminated Here');
+        
         const checkUser = await User.findOne({ where: {email: email}});
 
         if(!checkUser) {
             return res.status(404).json({error: 'Email does not exist in this system'});
         }
-
-        const client = sib.ApiClient.instance;
-        const apiKey = client.authentications['api-key'];
-        apiKey.apiKey = process.env.BREVO_KEY;
-
-        const emailAPI = new sib.TransactionalEmailsApi();
+        
+        const apiKey = process.env.BREVO_KEY
+        const url = 'https://api.brevo.com/v3/smtp/email';
 
         const emailData = {
-            sender: {name: "Expense Tracker Beta", email: "ayush.yadav.dev610@gmail.com"},
-            to: [{ email: email}, {email: "default5525@gmail.com"}],
-            subject: "Test mail",
-            htmlContent:`<h1>Check Data</h1>`
-        };
+            sender: { name:"Expense Tracker App", email:'ayush.yadav.dev610@gmail.com'},
+            to: [{ email: email}],
+            subject: "Test Mail",
+            htmlContent: '<html><body><h1>Check if email arrived</h1></body></html>'
+        }
+        console.log("CHECK 1");
+        const response = await axios.post(url, emailData, {
+            headers : {
+                'Content-Type': 'application/json',
+                'api-Key':apiKey
+            }
+        });
+        console.log('IT REACHES HERE!')
+        console.log(response);
+        if(response.ok){
+            console.log('Email Sent successfully!');
+        }
 
-        const response = await emailAPI.sendTransacEmail(emailData);
-        console.log('Link sent successfully:',response);
     } catch (error) {
         console.error('Error sending Email:', error);
     }
